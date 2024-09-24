1795eff62244bcdb17905fa0832445f6af7f9e43
K. Chavez <kchavez.dev@gmail.com>
2024-09-23 23:27:23 -0700

    Update PluginManagerEx.d.ts
    Add createCommandArgs

--- include/PluginManagerEx.d.ts
+++ include/PluginManagerEx.d.ts
@@ -2,6 +2,7 @@ interface IPluginManagerEx {
     createParameter(currentScript: HTMLOrSVGElement): NonNullable<any>
     findMetaValue(object: NonNullable<any>, nameList: string | string[]): any
     convertVariables(text: string, data?: any): any
+    createCommandArgs(args: any): any
 }
 
 declare var PluginManagerEx: IPluginManagerEx | undefined
